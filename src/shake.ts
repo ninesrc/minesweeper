8588fe89ede5e0ea658bbaa60979141f2b38b378
ninesrc <114354645+ninesrc@users.noreply.github.com>
2024-11-21 16:18:17 -0500

    Imagine
    no explanation needed

--- src/shake.ts
+++ src/shake.ts
@@ -1,11 +1,11 @@
 import { State } from "./types";
 
 export function shake(state:State, count: number) {
-    document.body.style.transform = "translate(" + (Math.random() * 3 - 1.5) + "%," + (Math.random() * 3 - 1.5) + "%)";
+    document.body.style.transform = "translate(" + (Math.random() * 2 - 1) + "%," + (Math.random() * 2 - 1) + "%)";
     if (count >= 0) {
       setTimeout(function () {
         shake(state, count - 1);
-      }, 10);
+      }, 7);
     } else {
       document.body.style.transform = "";
     }
