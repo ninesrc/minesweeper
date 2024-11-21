d5c46854c234516c5c9362a5a3651962ebced980
ninesrc <114354645+ninesrc@users.noreply.github.com>
2024-11-21 16:19:08 -0500

    No
    same thing here

--- src/shake.ts
+++ src/shake.ts
@@ -1,7 +1,7 @@
 import { State } from "./types";
 
 export function shake(state:State, count: number) {
-    document.body.style.transform = "translate(" + (Math.random() * 2 - 1) + "%," + (Math.random() * 2 - 1) + "%)";
+    document.body.style.transform = "translate(" + (Math.random() * 1 - 0.5) + "%," + (Math.random() * 1 - 0.5) + "%)";
     if (count >= 0) {
       setTimeout(function () {
         shake(state, count - 1);
